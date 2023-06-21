9281df98e815bb3289e623955b797216f1285630
nyarogithub <majiwaroysam046@gmail.com>
2023-06-21 07:42:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- *_putchar - writes the character c to sdtout
- *@c: The character to be printed
- *Return: On success 0.
- */
+*_putchar - writes the character c to sdtout
+*c: The character to be printed
+*Return: On success 0.
+*/
 
 int _putchar(char c)
 {
